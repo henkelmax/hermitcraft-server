f671dd7b86b35677b0cf6e84efe8624c80a878a3
Max Henkel <max@maxhenkel.de>
2024-12-10 18:50:00 +0100

    Create utils folder

--- sort.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-jq --sort-keys '.' modrinth.index.json > tmp.json && mv tmp.json modrinth.index.json
-
-jq '.files |= sort_by(.downloads)' modrinth.index.json > tmp.json && mv tmp.json modrinth.index.json
